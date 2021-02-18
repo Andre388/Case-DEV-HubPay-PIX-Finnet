ca90b7e21d5881cf52841089bcd8c0e0b713042d
Andre388 <76452851+Andre388@users.noreply.github.com>
2021-02-18 16:39:31 -0300

    Update cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -1,3 +1,9 @@
+<!Doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<head>
+<body>    
 <div id="cabecalho_superior">
     <!-- Criando o Nav de navegação -->
     <nav id="menu-cima">
@@ -14,36 +20,53 @@
 
 <div id="cabecalho_meio" class="fundo_azul"> <!-- a classe com a propriedade fundo azul e chamada --> 
 
-    <h1> As Melhores Ideias de andre estão aqui </h1>
+    <h1> As Melhores Ideias de andre estão aqui </h1><!-- com  a tag text-indent="-99999" no css eu oculto esse paragrafo.
 
     <!-- Foi criada uma div com a classe sacola para o paragrafo --> 
     <div id="div do paragrafo" class="sacola">
 
-    <p> Nenhum item no seu carrinho de compras </p>
+    <p id="paragrafo"> Nenhum item no seu carrinho de compras </p> <!-- editado no css --> 
 
     </div><!-- Fim da div paragrafo --> 
 
     <!-- inicio da minha section -->
     <section class="busca">    
     <!-- Inicio da Form -->    
-<form action="#">
-    <label>
-        <span id="span"> Busca </span>
-        <input type="search"  name="pesquisa" id="pesquisa" placeholder="Pesquisar" lang="pt-br">
-        <input type="button" id="botão de pesquisa" name="botão" value=" Iniciar Busca ">
-    </label>     
-</form>         
-</section> 
+
+        <form action="">
+    
+            <label><!-- inicio meu label -->
+        
+                <span id="span"> Busca </span>
+        
+                <input type="search"  name="pesquisa" id="pesquisa" placeholder="Pesquisar" lang="pt-br">
+        
+                <input type="button" id="botão de pesquisa" name="botão" value=" Iniciar Busca ">
+    
+            </label><!-- fecho o meu label -->     
+
+        </form><!--fecho o meu form -->         
+
+    </section><!-- fecho o meu section, que foi aberto com a class="busca" que sera usado no css as propriedades -->
+    
+
 </div><!--fecha cabecalho meio-->
-<div id="cabecalho_inferior" class="fundo_do_inferior">
-    <nav id="menu-principal">
+
+<div id="cabecalho_inferior" class="fundo_do_inferior"> <!--Inicio da div cabecalho inferior -->
+    <nav id="menu-principal"><!--inicio da nav do menu Principal"-->
+        
         <ul>
+            
             <li class="linha-vertical"><a href="#"><i>  Nome </i></a></li>
             <li class="linha-vertical"><a href="#"><i>  Produtos </i></a> </li>
             <li class="linha-vertical"><a href="#"><i> Fale Conosco </i></a> </li>
             <li class="linha-vertical"><a href="#">  Quem Somos  </a> </li>
-        </ul>    
+        
+        </ul>
+        
+     </nav><!-- fim da nav -->
 
-    </nav><!-- fim da nav -->
-
-</div><!--fecha cabecalho inferior-->    
+</div><!--fecha cabecalho_inferior-->
+    </body>
+    </html>
+    
