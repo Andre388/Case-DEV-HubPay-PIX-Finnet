7d2c8ad2b46583733f4276188bdc775adb7ff1c0
Andre388 <76452851+Andre388@users.noreply.github.com>
2021-02-19 09:05:18 -0300

    Update home.php

--- home.php
+++ home.php
@@ -1,6 +1,39 @@
-<?php
-// pagina home do meu site
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title> E-commerce do Andrezinho.com.br </title>
+  <link rel="stylesheet" href="css/css.css">
+</head>
+<body>
+<div id="corpo-loja"> <!-- inicio da div corpo de loja -->
+<aside class="banner"><img src="imagens/banner-meio.png"
+</div><!-- fim da div corpo de loja -->
+  
+  <section class="categoria">
+  <h2> Categorias </h2>
+    <nav>
+      <ul><!-- primeiro ul categorias 01 ao 03 -->
+        <li> <a href="#"> Categoria 01 </a></li>
+            <ul>
+              <li> <a href="#">Produto 01</a></li>
+              <li> <a href="#">Produto 02</a></li>
+              <li> <a href="#">Produto 03</a></li>
+            </ul>
+      <ul><!-- segunda ul de categorias -->
+              <li> <a href="#">Produto 04 </a></li>
+              <li> <a href="#">Produto 05 </a></li>
+              <li> <a href"#">Produto  06 </a></li>
+      <ul><!-- terceira ul de categorias -->
+              <li> <a href="#">Produto 07 </a></li>
+              <li> <a href="#">Produto 08 </a></li>
+              <li> <a href="#">Produto 09 </a></li>
+              <li> <a href="#">Produto 10 </a></li>
+            </ul> 
+    </nav>  
+  </section><!-- fim da section de categorias -->  
+  
+  
+<body>
+<html>  
+  
