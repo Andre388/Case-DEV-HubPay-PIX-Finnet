459415e69c67955e64232f91e1f6ee68827b0197
Andre388 <76452851+Andre388@users.noreply.github.com>
2021-02-19 15:03:44 -0300

    Update home.php

--- home.php
+++ home.php
@@ -10,26 +10,39 @@
 <aside class="banner"><img src="imagens/banner-meio.png">
 </div><!-- fim da div corpo de loja -->
   
+<!------------------------------------------------------------------------------------------>  
   <section class="categoria">
   <h2> Categorias </h2>
-    <nav>
-      <ul><!-- primeiro ul categorias 01 ao 03 -->
-        <li> <a href="#"> Categoria 01 </a></li>
-            <ul>
-              <li> <a href="#">Produto 01</a></li>
-              <li> <a href="#">Produto 02</a></li>
-              <li> <a href="#">Produto 03</a></li>
-            </ul>
-      <ul><!-- segunda ul de categorias -->
-              <li> <a href="#">Produto 04 </a></li>
-              <li> <a href="#">Produto 05 </a></li>
-              <li> <a href"#">Produto  06 </a></li>
-      <ul><!-- terceira ul de categorias -->
-              <li> <a href="#">Produto 07 </a></li>
-              <li> <a href="#">Produto 08 </a></li>
-              <li> <a href="#">Produto 09 </a></li>
-              <li> <a href="#">Produto 10 </a></li>
-            </ul><!-- fim do meu menu com nav --> 
+    <nav> <!-- INICIO DA NAV -->
+      
+<ul> <!--categoria 01 -->
+<li> <a href="#"> Categoria 01:</a></li>
+</ul><!--fecha categoria 01-->
+<ul> <!-- Inicio Bloco Produtos 01 -->
+   <li> <a href="#">Produto 01</a></li>
+   <li> <a href="#">Produto 02</a></li>
+   <li> <a href="#">Produto 03</a></li>
+</ul> <!-- fecha Produtos 01 -->
+ <!------------------------------------------------------------------------------------------->     
+<ul> <!--categoria 02 -->
+<li> <a href="#"> Categoria 02 </a></li>  
+</ul> <!-- fim categoria 02 -->
+<ul> <!-- Inicio Produtos 02 -->
+   <li> <a href="#">Produto 04 </a></li>
+   <li> <a href="#">Produto 05 </a></li>
+   <li> <a href="#">Produto 06 </a></li>
+</ul> <!-- fecha Produtos 02 -->
+<!------------------------------------------------------------------------------------------->
+<ul> <!--categoria 03 -->
+<li> <a href="#"> Categoria 03 </a></li>
+</ul><!--fim categoria 03 -->
+<ul><!-- Inicio Produtos 03 -->
+   <li> <a href="#">Produto 07 </a></li>
+   <li> <a href="#">Produtos08 </a></li>
+   <li> <a href="#">Produto 09 </a></li>
+   <li> <a href="#">Produto 10 </a></li>
+</ul><!-- fecha Produtos 03 --> 
+<!------------------------------------------------------------------------------------------->        
     </nav><!-- fim da nav -->  
   </section><!-- fim da section de categorias -->  
   
