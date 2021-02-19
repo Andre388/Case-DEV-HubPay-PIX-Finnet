7bb3aa6f5836364dbfee2e87c669b6fb46f641cb
Andre388 <76452851+Andre388@users.noreply.github.com>
2021-02-19 08:41:13 -0300

    update do index.php

--- index.php
+++ index.php
@@ -3,29 +3,20 @@
 <head>
     <meta charset="utf-8">
     <title>www.E-commerce do Andrezinho.com.br </title>
-	<link href="css/css.css" rel="stylesheet"/>
-	<?php include_once("cabecalho.php"); ?> <!-- temos o arquivo que compõe o cabecalho com a margem e menus do meu site -->
-	</head>
+<link href="css/css.css" rel="stylesheet"/>
+</head>
 <body>
-    <div id="Principal">
-
-      	<section id="cabecalho">
-		  
+<div id="Principal">
+     	<section id="cabecalho">
+		<?php include_once("cabecalho.php"); ?> <!-- temos o arquivo que compõe o cabecalho com a margem e menus do meu site -->
       	</section><!-- fim do topo e cabecalho -->
-
       	<section id="corpo"> <!-- inicio do corpo do meu arquivo -->
 		<?php include_once("home.php") ?> <!-- Temos o Arquivo que vai compor a parte central do meu site -->
 		
-
-      	</section><!-- fim corpo -->
-
+     	</section><!-- fim corpo -->
       	<footer id="rodape">
-			 
-
       	</footer><!-- Fim Rodape -->
-      		
-      		
-      		
+		
 </div><!-- Fim Principal -->		   
 
 </body>   
